0453c895bc472399a39ad440c7378d61558c3086
Rone Falcão <jose.rone@safeweb.com>
2024-05-01 11:17:50 -0300

    scrollando, posicionando e assinando, faltando calibrar a assinatura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { PdfViewerComponent, PDFDocumentProxy } from 'ng2-pdf-viewer';
 import { PDFDocument } from 'pdf-lib';
 
@@ -7,19 +14,19 @@ import { PDFDocument } from 'pdf-lib';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   isLoading: boolean = false;
   currentPage: number = 1;
 
   pdfSrc = '/assets/politica.pdf'; // Caminho do PDF
   @ViewChild('signatureBox') signatureBox!: ElementRef;
-  @ViewChild(PdfViewerComponent, { static: false })
-  pdfViewer!: PdfViewerComponent;
   @ViewChild('pdfWrapper') pdfWrapper!: ElementRef;
+  @ViewChild(PdfViewerComponent)
+  private pdfViewerComponent!: PdfViewerComponent;
 
   selectedPage: string = '1';
   signatureLeft: number = 0;
-  signatureTop: number = 0;
+  signatureTop: number = 650;
   isDragging: boolean = false;
   startX: number = 0;
   startY: number = 0;
@@ -28,11 +35,16 @@ export class AppComponent {
   scrollThreshold: number = 50; // Limite para ativar a rolagem
   pdf: PDFDocumentProxy | null = null; // Instância do PDF após o carregamento
 
-  constructor() {}
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.loadPDF();
   }
+
+   onPageRendered(e: any) {
+    this.currentPage = e.pageNumber;
+    console.log(`Current page: ${e.pageNumber}`);
+  }  
 
   loadPDF() {
     this.isLoading = true;
@@ -46,57 +58,26 @@ export class AppComponent {
     return `Left: ${this.signatureLeft}px, Top: ${this.signatureTop}px`;
   }
 
-  onMouseDown(event: MouseEvent) {
+  onMouseDown(event: MouseEvent): void {
     this.isDragging = true;
     this.startX = event.clientX;
     this.startY = event.clientY;
     this.startLeft = this.signatureLeft;
     this.startTop = this.signatureTop;
-    console.log(this.currentPage);
   }
 
-  async onMouseMove(event: MouseEvent) {
+  onMouseMove(event: MouseEvent): void {
     if (!this.isDragging) return;
-  
+
     const offsetX = event.clientX - this.startX;
     const offsetY = event.clientY - this.startY;
-  
     this.signatureLeft = this.startLeft + offsetX;
     this.signatureTop = this.startTop + offsetY;
-  
-    // Calcular a posição do carimbo em relação ao contêiner do PDF
-    const pdfWrapper = this.pdfWrapper.nativeElement;
-    const rect = pdfWrapper.getBoundingClientRect();
-    const pdfY = this.signatureTop - rect.top;
-  
-    // Calcular a altura de uma página do PDF
-    const pageHeight = rect.height / await this.getTotalNumberOfPages(); // Esperar a promessa ser resolvida
-  
-    // Calcular a página atual com base na posição vertical do carimbo
-    const currentPage = Math.ceil(pdfY / pageHeight);
-    const totalPages = await this.getTotalNumberOfPages(); // Esperar a promessa ser resolvida
-    if (currentPage >= 1 && currentPage <= totalPages) {
-      this.currentPage = currentPage;
-    }
-  
-    // Prevent default behavior to avoid text selection or other side effects
-    event.preventDefault();
-  }
-  
 
-  async getTotalNumberOfPages(): Promise<number> {
-    try {
-      const pdfData = await fetch(this.pdfSrc);
-      const arrayBuffer = await pdfData.arrayBuffer();
-      const pdfDoc = await PDFDocument.load(new Uint8Array(arrayBuffer));
-      return pdfDoc.getPageCount();
-    } catch (error) {
-      console.error('Error loading PDF:', error);
-      return 0; // Retorna 0 em caso de erro
-    }
+    event.preventDefault(); // Use 'event'
   }
 
-  onMouseUp(event: MouseEvent) {
+  onMouseUp(event: MouseEvent): void {
     this.isDragging = false;
   }
 
@@ -108,17 +89,25 @@ export class AppComponent {
       const pdfDoc = await PDFDocument.load(existingPdfBytes);
       const pages = pdfDoc.getPages();
       const currentPageIndex = this.currentPage - 1; // Convertendo de 1-base para 0-base
-
-      // Utilize currentPageIndex para acessar a página correta
       const page = pages[currentPageIndex];
+
+      if (!page) {
+        console.error('Page not found');
+        return;
+      }
 
       const imageUrl = '/assets/carimbo.png';
       const imageBytes = await fetch(imageUrl).then((res) => res.arrayBuffer());
       const image = await pdfDoc.embedPng(imageBytes);
 
+      if (!image) {
+        console.error('Failed to load or embed the image');
+        return;
+      }
+
       page.drawImage(image, {
-        x: this.signatureLeft, // Coordenadas x da assinatura
-        y: page.getHeight() - this.signatureTop - 66, // Coordenadas y da assinatura
+        x: this.signatureLeft,
+        y: page.getHeight() - this.signatureTop - image.height - 90,
         width: image.width,
         height: image.height,
       });
